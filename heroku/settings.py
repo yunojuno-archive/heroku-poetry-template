4e4d23892ce88bbdbf011563ef09108605ae373e
Hugo Rodger-Brown <hugo@yunojuno.com>
2022-08-12 13:24:24 +0100

    Fix settings (remove LINEAR)

--- heroku/settings.py
+++ heroku/settings.py
@@ -19,8 +19,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(PROJECT_DIR, "../", "staticfiles")  # noqa
 
-LOGGING["loggers"]["linear"]["level"] = os.environ.get(  # type: ignore # noqa: F405
-    "LOGGING_LEVEL_LINEAR", "INFO"
-)
-
 DEBUG = False
